74a3822ac46caf5981c828e0fe14be41492a5ff4
Marc <marcedforlife@gmail.com>
2025-02-19 10:44:54 +1300

    Added the user.js file for default preferences

---
+++ src/user.js
@@ -0,0 +1,5 @@
+// Display the find/search bar at the top of the window
+user_pref("floorp.search.top.mode", true);
+
+// Enable picture in picture video control
+user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", true);
